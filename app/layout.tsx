0e623f41c374e592d6cd7a52c6c47391bdebb421
Baptiste Georget <baptiste.georget@proton.me>
2024-07-21 17:59:39 +0200

    Update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "SUPWARDEN",
-  description: "Password manager by SUPWARDEN.",
-  icons: {
-    icon:  '/icon.svg'
-  }
-};
+import "./globals.css"
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className="h-screen w-full bg-black text-white flex flex-col">{children}</body>
+      <body className="h-screen w-full bg-black text-white flex flex-col">
+        {children}
+      </body>
     </html>
-  );
+  )
 }
