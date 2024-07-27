d8f4f4ebd751779d3a7da7c9d95b91ca013fb70b
Baptiste Georget <baptiste.georget@proton.me>
2024-07-27 22:19:20 +0200

    Update buttons

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,30 +1,54 @@
-export function PrimaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
+export function PrimaryButton({
+  children,
+  onClick,
+  justify
+}: {
+  children?: React.ReactNode,
+  onClick?: () => void,
+  justify: "justify-center" | "justify-start" | "justify-end"
+}) {
   return (
     <button
       onClick={onClick}
-      className={`bg-white text-black px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 ${className}`}
+      className={`bg-white text-black px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 ${justify}`}
     >
       {children}
     </button>
   )
 }
 
-export function SecondaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
+export function SecondaryButton({
+  children,
+  onClick,
+  justify
+}: {
+  children?: React.ReactNode,
+  onClick?: () => void,
+  justify: "justify-center" | "justify-start" | "justify-end"
+}) {
   return (
     <button
       onClick={onClick}
-      className={`bg-black text-white px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 border border-neutral-700 ${className}`}
+      className={`bg-black text-white px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 border border-neutral-700 ${justify}`}
     >
       {children}
     </button>
   )
 }
 
-export function TertiaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
+export function TertiaryButton({
+  children,
+  onClick,
+  justify
+}: {
+  children?: React.ReactNode,
+  onClick?: () => void,
+  justify: "justify-center" | "justify-start" | "justify-end"
+}) {
   return (
     <button
       onClick={onClick}
-      className={`bg-transparent text-white px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 ${className}`}
+      className={`bg-transparent text-white px-2 py-1 rounded-md shadow-lg flex-1 flex items-center gap-2 ${justify}`}
     >
       {children}
     </button>
