9ecc2d594f34edb1d8b149535b58a8a3f56f1857
Baptiste Georget <baptiste.georget@proton.me>
2024-07-27 18:53:45 +0200

    Update components buttons

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,17 +1,17 @@
 export function PrimaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
   return (
-    <button onClick={onClick} className={`${className} bg-white text-black px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 justify-center`}>{children}</button>
+    <button onClick={onClick} className={`bg-white text-black px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 ${className}`}>{children}</button>
   )
 }
 
 export function SecondaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
   return (
-    <button onClick={onClick} className={`${className} bg-black text-white px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 justify-center border border-neutral-700`}>{children}</button>
+    <button onClick={onClick} className={`bg-black text-white px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 border border-neutral-700 ${className}`}>{children}</button>
   )
 }
 
 export function TertiaryButton({ children, onClick, className }: { children?: React.ReactNode, onClick?: () => void, className?: string }) {
   return (
-    <button onClick={onClick} className={`${className} bg-transparent text-white px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 justify-center`}>{children}</button>
+    <button onClick={onClick} className={`bg-transparent text-white px-4 py-2 rounded-md shadow-lg flex-1 flex items-center gap-2 ${className}`}>{children}</button>
   )
 }
