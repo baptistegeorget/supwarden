f495c718ec5f0f4a3f7c801331b8c06a79e6a534
Baptiste Georget <baptiste.georget@proton.me>
2024-07-27 17:02:56 +0200

    Update sheme and types

--- types.d.ts
+++ types.d.ts
@@ -3,4 +3,36 @@ export type User = {
   firstName: string,
   email: string,
   password: string,
+  pin?: string,
+}
+
+export type Folder = {
+  name: string,
+  type: "personal" | "shared",
+  members?: string[],
+  createdBy: string,
+  createdOn: string,
+  modifiedBy: string,
+  modifiedOn: string,
+}
+
+export type Element = {
+  folder: string,
+  name: string,
+  identifier?: string,
+  password?: string,
+  urls?: string[],
+  note?: string,
+  customFields?: CustomField[],
+  usersWhoCanEdit?: string[],
+  isSensitive?: boolean,
+  createdBy: string,
+  createdOn: string,
+  modifiedBy: string,
+  modifiedOn: string,
+}
+
+type CustomField = {
+  type: "visible" | "hidden" | "attachment",
+  value: string,
 }
