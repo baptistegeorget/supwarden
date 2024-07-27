24f2a0ea90d6f8cb6ecc9acd8b4ab906978fa972
Baptiste Georget <baptiste.georget@proton.me>
2024-07-27 17:08:15 +0200

    Add component popup for create folder

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,12 +4,18 @@ import { cookies } from "next/headers"
 import jwt from "jsonwebtoken"
 import { User } from "@/types"
 import { redirect } from "next/navigation"
+import { WithId } from "mongodb"
 
 if (!process.env.JWT_SECRET) {
   throw new Error('Invalid/Missing environment variable: "JWT_SECRET"')
 }
 
+if (!process.env.BASE_URL) {
+  throw new Error('Invalid/Missing environment variable: "BASE_URL"')
+}
+
 const jwtSecret = process.env.JWT_SECRET
+const baseUrl = process.env.BASE_URL
 
 export async function setAuthTokenCookie(authToken: string) {
   cookies().set("auth-token", authToken)
@@ -28,10 +34,50 @@ export async function auth() {
       return null
     }
 
-    const authUser = jwt.verify(authToken, jwtSecret) as User
+    const authUser = await decodeAuthToken(authToken)
 
     return authUser
   } catch (error) {
     return null
+  }
+}
+
+export async function decodeAuthToken(authToken: string) {
+  try {
+    const authUser = jwt.verify(authToken, jwtSecret) as WithId<User>
+    return authUser
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      redirect("/signin")
+    }
+    throw new Error("Invalid token")
+  }
+}
+
+export async function createSharedFolder(formData: FormData) {
+  const authToken = cookies().get("auth-token")?.value
+
+  if (!authToken) {
+    redirect("/signin")
+  }
+
+  const data = {
+    name: formData.get("name") as string,
+  }
+
+  const response = await fetch(`${baseUrl}/api/folders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authToken,
+    },
+    body: JSON.stringify(data),
+  })
+
+  if (response.ok) {
+    return null
+  } else {
+    const result = await response.json()
+    return result.error
   }
 }
