a8d6c3c1c43bebac3e44e91086e8873bde224ffa
Baptiste Georget <baptiste.georget@proton.me>
2024-07-27 20:05:28 +0200

    Add folder scheme to zod

--- lib/zod.ts
+++ lib/zod.ts
@@ -25,3 +25,9 @@ export const signUpSchema = object({
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 })
+
+export const folderSchema = object({
+  name: string({ required_error: "Name is required" })
+    .min(1, "Name is required")
+    .max(32, "Name must be less than 32 characters"),
+})
