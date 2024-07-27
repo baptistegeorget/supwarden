325552bb998306947d2312e90f35d857325cb518
Baptiste Georget <baptiste.georget@proton.me>
2024-07-28 00:14:42 +0200

    Update sign in and sign up pages

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { SignInForm } from "@/components/forms"
 import { auth } from "@/lib/auth"
 import { redirect } from "next/navigation"
+import { WebSiteTitle } from "@/components/miscellaneous"
 
 export default async function SignInPage() {
   const session = await auth()
@@ -13,7 +14,7 @@ export default async function SignInPage() {
   return (
     <main className="flex-1 flex flex-col justify-center items-center">
       <div className="rounded-md shadow-lg py-8 px-16 flex flex-col gap-4 border border-neutral-700">
-        <h1 className="text-center text-2xl">SUPWARDEN</h1>
+        <WebSiteTitle>SUPWARDEN</WebSiteTitle>
         <SignInForm />
         <Link href="/signup" className="text-center text-blue-600">I don&apos;t have an account</Link>
         <Link href="/forgot-password" className="text-center text-blue-600">I forgot my password</Link>
