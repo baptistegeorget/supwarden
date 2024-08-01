776c29600fc37b758684c85631539d615ce4683a
Baptiste Georget <baptiste.georget@proton.me>
2024-08-01 18:32:25 +0200

    Update pages

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -12,13 +12,12 @@ export default async function SignInPage() {
   }
 
   return (
-    <main className="flex-1 flex flex-col justify-center items-center">
-      <div className="rounded-md shadow-lg py-8 px-16 flex flex-col gap-4 border border-neutral-700">
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <main className="rounded-md shadow-lg py-8 px-16 flex flex-col gap-4 border border-neutral-700">
         <WebSiteTitle>SUPWARDEN</WebSiteTitle>
         <SignInForm />
         <Link href="/signup" className="text-center text-blue-600">I don&apos;t have an account</Link>
-        <Link href="/forgot-password" className="text-center text-blue-600">I forgot my password</Link>
-      </div>
-    </main>
+      </main>
+    </div>
   )
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -12,12 +12,12 @@ export default async function SignUpPage() {
   }
 
   return (
-    <main className="flex-1 flex flex-col justify-center items-center">
-      <div className="rounded-md shadow-lg py-8 px-16 flex flex-col gap-4 border border-neutral-700">
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <main className="rounded-md shadow-lg py-8 px-16 flex flex-col gap-4 border border-neutral-700">
         <WebSiteTitle>SUPWARDEN</WebSiteTitle>
         <SignUpForm />
         <Link href="/signin" className="text-center text-blue-600">I have an account</Link>
-      </div>
-    </main>
+      </main>
+    </div>
   )
 }
