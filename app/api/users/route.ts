4334d96011f98ead328eb91b1269a503d9745b0c
Baptiste Georget <baptiste.georget@proton.me>
2024-08-01 18:28:09 +0200

    Update routes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -47,5 +47,6 @@ export async function POST(request: Request) {
     if (error instanceof Error) {
       return Response.json({ error: error.message }, { status: 400 })
     }
+    return Response.json({ error: "An error occurred" }, { status: 500 })
   }
 }
