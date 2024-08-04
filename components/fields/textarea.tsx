84115cfc2e3fa35656293351e18ac2ae19db7443
Baptiste Georget <baptiste.georget@proton.me>
2024-08-04 14:52:33 +0200

    Update text area fields

--- components/fields/textarea.tsx
+++ components/fields/textarea.tsx
@@ -23,8 +23,7 @@ export default function TextAreaField({
         required={required}
         minLength={minLength}
         maxLength={maxLength}
-      >
-      </textarea>
+      />
     </div>
   )
 }
