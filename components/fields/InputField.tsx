6e3ad9fc378209ff9c5e39b2b73dd7d985a514f8
Baptiste Georget <baptiste.georget@proton.me>
2024-08-11 21:54:33 +0200

    Update fields

--- components/fields/InputField.tsx
+++ components/fields/InputField.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { HTMLInputTypeAttribute } from "react"
 
 export default function InputField({
--- components/fields/TextAreaField.tsx
+++ components/fields/TextAreaField.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 export default function TextAreaField({
   label,
   name,
