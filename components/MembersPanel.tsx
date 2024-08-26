e25d271a424bce5bba2d349141632a88b38f4407
Baptiste Georget <baptiste.georget@proton.me>
2024-08-26 17:37:17 +0200

    Update members panel

--- components/MembersPanel.tsx
+++ components/MembersPanel.tsx
@@ -33,7 +33,7 @@ export default function MembersPanel({
   }
 
   return (
-    <aside className="py-4 px-8 border-l border-neutral-700 w-96 overflow-auto scrollbar-thin">
+    <aside className="p-8 border-l border-neutral-700 w-96 overflow-auto scrollbar-thin">
       {members.map((member) => (
         <div key={member.id} className="flex gap-2 items-center">
           <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.754 14a2.249 2.249 0 0 1 2.25 2.249v.918a2.75 2.75 0 0 1-.513 1.599C17.945 20.929 15.42 22 12 22c-3.422 0-5.945-1.072-7.487-3.237a2.75 2.75 0 0 1-.51-1.595v-.92a2.249 2.249 0 0 1 2.249-2.25h11.501ZM12 2.004a5 5 0 1 1 0 10 5 5 0 0 1 0-10Z" fill="#ffffff" /></svg>
