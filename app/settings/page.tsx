acdd8e4409e58af9feb83c07a905b746f8f66169
Baptiste Georget <baptiste.georget@proton.me>
2024-08-26 17:16:02 +0200

    Update settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,30 +1,25 @@
 "use client"
 
-import Header from "@/components/header"
-import { useNotification } from "@/components/providers/NotificationProvider"
-import { auth } from "@/lib/auth"
+import Header from "@/components/Header"
+import { getSession } from "@/lib/auth"
 import { SessionResponse } from "@/types"
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function SettingsPage() {
-  const notify = useNotification()
   const router = useRouter()
   const [session, setSession] = useState<SessionResponse | null>(null)
 
   useEffect(() => {
-    getSession(router)
-  }, [])
-
-  async function getSession(router: AppRouterInstance) {
-    const session = await auth()
-    if (session) {
-      return setSession(session)
-    } else {
-      return router.push("/signin")
-    }
-  }
+    getSession()
+      .then((session) => {
+        if (session) {
+          setSession(session)
+        } else {
+          router.push("/signin")
+        }
+      })
+  }, [router])
 
   if (!session) return null
 
