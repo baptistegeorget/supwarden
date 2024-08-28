fd446209f9c5c2a2b48d8174bf2adebc4898c7cd
Baptiste Georget <baptiste.georget@proton.me>
2024-08-28 16:35:24 +0200

    Update folders panel

--- components/FoldersPanel.tsx
+++ components/FoldersPanel.tsx
@@ -4,14 +4,15 @@ import { useNotification } from "@/components/providers/NotificationProvider"
 
 export default function FoldersPanel({
   session,
+  selectedFolder,
   onSelect
 }: {
   session: SessionResponse,
+  selectedFolder?: FolderResponse,
   onSelect?: (folder: FolderResponse) => void
 }) {
   const notify = useNotification()
   const [folders, setFolders] = useState<FolderResponse[]>([])
-  const [selectedFolder, setSelectedFolder] = useState<FolderResponse | undefined>(undefined)
 
   useEffect(() => {
     fetchFolders(session.user.id)
@@ -69,10 +70,7 @@ export default function FoldersPanel({
         {folders.map((folder) => (
           <button
             key={folder.id}
-            onClick={() => {
-              setSelectedFolder(folder)
-              if (onSelect) onSelect(folder)
-            }}
+            onClick={() => onSelect?.(folder)}
             className={`flex gap-2 items-center px-2 py-1 rounded w-full ${folder.id === selectedFolder?.id ? "bg-white text-black" : "bg-black text-white"}`}
           >
             <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.821 6.5h5.929a2.25 2.25 0 0 1 2.229 1.938l.016.158.005.154v9a2.25 2.25 0 0 1-2.096 2.245L19.75 20H4.25a2.25 2.25 0 0 1-2.245-2.096L2 17.75v-7.251l6.207.001.196-.009a2.25 2.25 0 0 0 1.088-.393l.156-.12L13.821 6.5ZM8.207 4c.46 0 .908.141 1.284.402l.156.12 2.103 1.751-3.063 2.553-.085.061a.75.75 0 0 1-.29.106L8.206 9 2 8.999V6.25a2.25 2.25 0 0 1 2.096-2.245L4.25 4h3.957Z" fill={folder.id === selectedFolder?.id ? "#000000" : "#ffffff"} /></svg>
